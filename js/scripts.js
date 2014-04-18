11c9223524c0587cf22636d502c297e2318caac8
Nabil Kashyap <n@nabilk.com>
2014-04-18 17:35:29 -0400

    very working

--- js/scripts.js
+++ js/scripts.js
@@ -3,41 +3,64 @@
 
 var instaShow = {
 
-	getPics: function(){
+	args: {
+			client_id: client_id,
+		},
 
-		var url = {
+	url: {
 			base: 'https://api.instagram.com/v1',
 			params: 'tags/swatspinepoems',
 			end: 'media/recent'
-		}
-		
-		var args = { 	
-			'client_id': client_id,
-	        	};
+		},
 
-		$.ajax({
+	makePics: function(){
+		
+		instaShow.getPics().done( function(d){
+
+	        for(i in d.data){
+
+				imgURL = {
+					base: 'http://instagr.am/p',
+					bug: d.data[i].link.slice(-11,-1),
+					end: 'media/?size=l'
+				}
+
+				var post = $('#container').append('<div class="post">')
+					.last('.post')
+		    		.append('<img src="' + implode('/', imgURL) + '" />')
+		    		.append('<p>' + d.data[i].caption.text);
+
+		    	$('#container').append(post);
+			}
+
+			$('#container .post:gt(0)').hide()
+			instaShow.animatePics();
+		});
+	},
+
+	getPics: function(){
+
+		return $.ajax({
 
 	        type: 'GET',
-	        url: implode('/', url),
-	        data: args,
+	        url: implode('/', instaShow.url),
+	        data: instaShow.args,
 	        dataType: 'jsonp',
-	        success: function(d) {
-	        	for(i in d.data){
-
-	        		tURL = {
-	        			base: 'http://instagr.am/p',
-	        			bug: d.data[i].link.slice(-11,-1),
-	        			end: 'media/?size=l'
-	        		}
-
-	        		$('body').append('<img src="' + implode('/', tURL) + '" />')
-	        		$('body').append('<p>' + d.data[i].caption.text)
-	        	}
-	        },
 	        error: function(e) {
 	            console.log(e.message);
 	        }
 	    });
+	},
+
+	animatePics: function(){
+
+		setInterval(function(){ 
+
+			$('#container :first-child').fadeOut()
+				.next('.post').fadeIn()
+				.end().appendTo('#container')
+
+		}, 3000);
 	}
 }
 
@@ -68,6 +91,6 @@ function implode(glue, pieces) {
   return pieces;
 }
 
-instaShow.getPics()
-
-// https://api.instagram.com/v1/users/swarthmorelibraries/media/recent/?callback=jQuery111008292809315700378_1397750556674&client_id=def944e656064c8185ead12a710cbd05&_=1397750556675
+$(document).ready(function(){
+	instaShow.makePics();
+});
